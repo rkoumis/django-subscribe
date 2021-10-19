50e91e100aedaddda9564e64ea7d908da94698ad
Robin Koumis <rkoumis@secureworks.com>
2021-10-19 15:18:12 -0400

    Change to subscribe/urls.py for Django 2

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,14 +1,14 @@
 """URLs for the ``subscribe`` app."""
-from django.conf.urls import url
+from django.urls import re_path
 
 from subscribe.views import SubscriptionCreateView, SubscriptionDeleteView
 
 
 urlpatterns = [
-    url(r'^create/(?P<ctype_pk>\d+)/(?P<object_pk>\d+)/$',
+    re_path(r'^create/(?P<ctype_pk>\d+)/(?P<object_pk>\d+)/$',
         SubscriptionCreateView.as_view(),
         name='subscriptions_create',),
-    url(r'^delete/(?P<ctype_pk>\d+)/(?P<object_pk>\d+)/$',
+    re_path(r'^delete/(?P<ctype_pk>\d+)/(?P<object_pk>\d+)/$',
         SubscriptionDeleteView.as_view(),
         name='subscriptions_delete',),
 ]
