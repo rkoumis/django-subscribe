fa543dfc386d7ebf37e3840bb28c789b3e88da49
Robin Koumis <rkoumis@secureworks.com>
2021-10-25 16:55:58 -0400

    Updated urls.py.

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,14 +1,14 @@
 """URLs for the ``subscribe`` app."""
-from django.urls import path
+from django.conf.urls import url
 
 from subscribe.views import SubscriptionCreateView, SubscriptionDeleteView
 
 
 urlpatterns = [
-    path('create/<int:ctype_pk>/<int:object_pk>/',
+    url(r'^create/(?P<type_pk>)/(?P<object_pk>)/',
         SubscriptionCreateView.as_view(),
         name='subscriptions_create',),
-    path('delete/<int:ctype_pk>/<int:object_pk>/',
+    url(r'^delete/(?P<ctype_pk>)/(^P<object_pk>)/',
         SubscriptionDeleteView.as_view(),
         name='subscriptions_delete',),
 ]
