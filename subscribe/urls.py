04b0aee27a5b054d8515af970dc571426642c923
Robin Koumis <rkoumis@secureworks.com>
2021-10-26 10:49:06 -0400

    Restore regular expression, loosen object_pk to match uuids; use re_path instead of url

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -1,14 +1,14 @@
 """URLs for the ``subscribe`` app."""
-from django.conf.urls import url
+from django.urls import re_path
 
 from subscribe.views import SubscriptionCreateView, SubscriptionDeleteView
 
 
 urlpatterns = [
-    url(r'^create/(?P<ctype_pk>)/(?P<object_pk>)/',
+    re_path(r'^create/(?P<ctype_pk>\d+)/(?P<object_pk>[\w-]+)/$',
         SubscriptionCreateView.as_view(),
         name='subscriptions_create',),
-    url(r'^delete/(?P<ctype_pk>)/(^P<object_pk>)/',
+    re_path(r'^delete/(?P<ctype_pk>\d+)/(?P<object_pk>[\w-]+)/$',
         SubscriptionDeleteView.as_view(),
         name='subscriptions_delete',),
 ]
